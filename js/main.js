147dff8ab855f51bf4ebc81b0b231dce9d499082
Aaron Bell <aaron.bell@gmail.com>
2014-05-21 18:24:50 -0400

    hypercube of cubes working, need to fix camera next

--- js/main.js
+++ js/main.js
@@ -5,7 +5,10 @@ var three = THREE.Bootstrap();
 
 var shaderMaterial = createShader();
 
-var geometry = createGeometry();
+var hypercubeSize = 20;
+var geometry = createGeometry(hypercubeSize);
+// assume cubes are 1x1x1 and 0.5 apart
+var cameraHeight = (hypercubeSize * 1.5) / 2;
 
 var mesh = new THREE.Mesh(geometry, shaderMaterial);
 
@@ -22,40 +25,53 @@ three.on('update', function () {
 
   animateShader(shaderMaterial, t);
 
-  var cameraDistance = 3.0;
-  var rotateSpeed = 0.3;
-  three.camera.position.set(Math.cos(t * rotateSpeed) * cameraDistance, 1.0, Math.sin(t * rotateSpeed) * cameraDistance);
+  var cameraDistance = 25.0;
+  var rotateSpeed = 0.2;
+
+  three.camera.position.set(Math.cos(t * rotateSpeed) * cameraDistance, cameraHeight, Math.sin(t * rotateSpeed) * cameraDistance);
   three.camera.lookAt(new THREE.Vector3());
 });
 
-function createGeometry()
+function createGeometry(cubesPerRow)
 {
   var geometry = new THREE.Geometry();  // start with nothing
-  var cubeGeometry = new THREE.CubeGeometry(1.0, 1.0, 1.0); // a reference cube
-  var numberOfCubes = 2;
-  var offsetX = 1.5;
+  var cubeSize = 1.0;
+  var cubeGeometry = new THREE.CubeGeometry(cubeSize, cubeSize, cubeSize); // a reference cube
+  var spacing = cubeSize + (cubeSize / 2);
+  var cubesCreated = 0;
 
-  for (var cube = 0; cube < numberOfCubes; cube++)
+  for (var cubeX = 0; cubeX < cubesPerRow; cubeX++)
   {
-    console.log('cube ' + cube);
-    var offset = cube * offsetX;
-    // add vertices
-    for (var vIndex = 0; vIndex < cubeGeometry.vertices.length; vIndex++)
+    for (var cubeY = 0; cubeY < cubesPerRow; cubeY++)
     {
-      var vertex = cubeGeometry.vertices[vIndex];
-      var arrSize = geometry.vertices.push(new THREE.Vector3(vertex.x + offset, vertex.y, vertex.z));
-      console.log(geometry.vertices[arrSize - 1]);
-    }
-    // add faces
-    for (var fIndex = 0; fIndex < cubeGeometry.faces.length; fIndex++)
-    {
-      var face = cubeGeometry.faces[fIndex];
-      var faceOffset = cube * cubeGeometry.vertices.length;
-      var arrSize = geometry.faces.push(new THREE.Face3(face.a + faceOffset, face.b + faceOffset, face.c + faceOffset));
-      console.log(geometry.faces[arrSize - 1]);
+      for (var cubeZ = 0; cubeZ < cubesPerRow; cubeZ++)
+      {
+        // console.log('creating cube ' + cubesCreated + ' at location ' + cubeX + ', ' + cubeY + ', ' + cubeZ);
+        var offsetX = cubeX * spacing;
+        var offsetY = cubeY * spacing;
+        var offsetZ = cubeZ * spacing;
+        // add vertices
+        for (var vIndex = 0; vIndex < cubeGeometry.vertices.length; vIndex++)
+        {
+          var vertex = cubeGeometry.vertices[vIndex];
+          geometry.vertices.push(new THREE.Vector3(vertex.x + offsetX, vertex.y + offsetY, vertex.z + offsetZ));
+          // console.log(geometry.vertices[geometry.vertices.length - 1]);
+        }
+        // add faces, i.e. sets of vertex indices
+        for (var fIndex = 0; fIndex < cubeGeometry.faces.length; fIndex++)
+        {
+          var face = cubeGeometry.faces[fIndex];
+          var faceOffset = cubesCreated * cubeGeometry.vertices.length;
+          geometry.faces.push(new THREE.Face3(face.a + faceOffset, face.b + faceOffset, face.c + faceOffset));
+          // console.log(geometry.faces[geometry.faces.length - 1]);
+        }
+
+        cubesCreated += 1;
+      }
     }
   }
-  console.log(geometry);
+  // console.log(geometry);
+  console.log('cubes: ' + cubesCreated);
   return geometry;
 }
 
@@ -86,7 +102,7 @@ function setNewColorAttribute(material, theMesh)
 
   var vertArray = theMesh.geometry.vertices;
 
-  var colorTable = new THREE.Lut('rainbow', 16);
+  var colorTable = new THREE.Lut('rainbow', 1024);
   colorTable.setMin(0);
   colorTable.setMax(vertArray.length - 1);
   console.log(colorTable);
@@ -110,5 +126,5 @@ function addReferenceShapes()
 
 function animateShader(material, timeNow)
 {
-  material.uniforms.amplitude.value = 1 + (Math.abs(Math.sin(timeNow) * 0.5));
+  material.uniforms.amplitude.value = 1 + (Math.abs(Math.sin(timeNow) * 0.0));
 }
